0364e5512c20b5bc9f2a8b85c143a0f81e83106d
Arto Bendiken <arto@bendiken.net>
2010-05-28 01:52:52 +0200

    Ensured that all known RDF formats are registered with Rack's MIME types registry upon initialization.

--- lib/rack/linkeddata.rb
+++ lib/rack/linkeddata.rb
@@ -5,5 +5,23 @@ module Rack
   module LinkedData
     autoload :ContentNegotiation, 'rack/linkeddata/conneg'
     autoload :VERSION,            'rack/linkeddata/version'
+
+    ##
+    # Registers all known RDF formats with Rack's MIME types registry.
+    #
+    # @param  [Hash{Symbol => Object}] options
+    # @option options [Boolean]        :overwrite (false)
+    # @return [void]
+    def self.register_mime_types!(options = {})
+      if defined?(Rack::Mime::MIME_TYPES)
+        RDF::Format.file_extensions.each do |file_ext, content_type|
+          if !Rack::Mime::MIME_TYPES.has_key?(file_ext = ".#{file_ext}") || options[:overwrite]
+            Rack::Mime::MIME_TYPES.merge!(file_ext => content_type.to_s)
+          end
+        end
+      end
+    end
   end
 end
+
+Rack::LinkedData.register_mime_types!
